20223696f8edcf849c100ff1bf46669e2b62cd0b
alvaroormeno <alvaro@ormeno.org>
2022-07-14 14:59:56 -0400

    finished whole design with active states

--- script/script.js
+++ script/script.js
@@ -1,13 +1,34 @@
 //get elements
 const shareContainer = document.querySelector(".share-container")
 const shareContainerArrow = document.querySelector(".share-container-arrow")
-const shareButton = document.querySelector(".share-box")
 
+const shareButton = document.querySelectorAll(".share-box")
+
+const userBoxMobile = document.querySelector(".user-box")
+const userBoxMobileActive = document.querySelector('.third-container-mobile')
+
+var x = window.matchMedia("(max-width: 390px)")
 
 showShareContainer = () => {
-    shareContainer.classList.toggle('share-container-active')
-    shareContainerArrow.classList.toggle('share-container-arrow-active')
-
+    if(x.matches) {
+        console.log('clicked mobile!!!!')
+        userBoxMobile.classList.toggle('user-box-close')
+        userBoxMobileActive.classList.toggle('third-container-mobile-active')
+        
+    } else {
+        console.log('clicked desktop!!!!')
+        shareContainer.classList.toggle('share-container-active')
+        shareContainerArrow.classList.toggle('share-container-arrow-active')
+    }
+    
 }
 
-shareButton.addEventListener("click", showShareContainer)
+shareButton.forEach((button) => {
+    button.addEventListener("click", showShareContainer)
+    
+})
+
+
+
+
+
